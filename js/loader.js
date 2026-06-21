d32067a9729126145b96c039980ad57e7da18529
ksg721 <gre20018@byui.edu>
2026-06-20 19:14:43 -0600

    new loader.json

--- js/loader.js
+++ js/loader.js
@@ -1,11 +1,11 @@
-const DATA_PATH = "../data/";
+const DATA_PATH = "./data/";
 
 export async function loadJSON(fileName) {
   try {
     const response = await fetch(`${DATA_PATH}${fileName}.json`);
 
     if (!response.ok) {
-      throw new Error(`Failed to load ${fileName}`);
+      throw new Error(`Failed to load ${fileName}: ${response.status}`);
     }
 
     return await response.json();
